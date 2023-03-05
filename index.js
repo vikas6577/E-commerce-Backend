641ca45074c6a1b08d08d90d0bc2febd3a74c44c
vikas6577 <vs509411@gmail.com>
2023-03-05 16:25:33 +0530

    signup api created

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express=require('express');
-
+require('./db/config');
+const User=require("./db/User")
 const app=express();
 
-app.get('/',(req,res)=>{
-    res.send("Welcome to e-dashboard")
+app.use(express.json());
+// when using app.post and access json data than and insert it in db
+
+app.post("/register",async(req,res)=>{
+    let user=new User(req.body);
+    let result=await user.save();
+    res.send(result)
 })
 
 app.listen(5000, function(err){
     if (err) console.log("Error in server setup")
     console.log("Server listening on Port", 5000);
 })
-// ...
